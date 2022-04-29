cddcc4fbd3b86092e0804ddc1c915f486b4340b7
MarkKrasikov <mark.krasikov.98@bk.ru>
2022-04-29 09:35:27 +0300

    coolout-crm-chat-bot

--- index.cjs
+++ index.cjs
@@ -10,82 +10,11 @@ const bot = new TelegramBot(token, { polling: true });
 var isShiftClosed = true;
 var category; // Отвечает за номер выбранной категории
 
+
+const { testFunc } = require('./menu.cjs');
+
 //Списки отвечающие за пункты меню
-var yesNo = [
-    { name: 'Да' },
-    { name: 'Нет' }
-];
-
-var openShift = [
-    { name: 'Открыть' }
-];
-
-var backOrderMenu = [
-    { name: 'В меню' },
-    { name: 'Отмена' }
-];
-
-var open = [
-    { name: 'Новый заказ' },
-    { name: 'Посмотреть статистику' },
-    { name: 'Закрыть смену' }
-];
-
-var newOrder = [
-    { name: 'Кофе' },
-    { name: 'Горячие напитки' },
-    { name: 'Холодные напитки' },
-    { name: 'Десерты' },
-    { name: 'Еда' },
-    { name: 'Выпечка' },
-    { name: 'Батончики' },
-    { name: 'Закрыть чек' },
-    { name: 'В главное меню' }
-];
-
-
-// Списки отвечающие за пункты таваров
-var listCoffee = [
-    { name: 'Американо', price: 4 },
-    { name: 'Маленький капучино', price: 4 },
-
-];
-
-var listHotDrinks = [
-    { name: 'Чаек', price: 4 },
-    { name: 'Какавчик', price: 5 },
-
-];
-
-var listColdDrinks = [
-    { name: 'Коктейльчик', price: 5.5 },
-    { name: 'Кола', price: 5, },
-
-];
-
-var listDeserts = [
-    { name: 'Медовик', price: 4.5 },
-    { name: 'Красный бархат', price: 5 },
-
-];
-
-var listFood = [
-    { name: 'Яишенка', price: 5 },
-    { name: 'Сэндвич', price: 6 },
-
-];
-
-var listBakery = [
-    { name: 'Крендель', price: 2.8 },
-    { name: 'Берлинер', price: 3 },
-
-];
-
-var listBars = [
-    { name: 'Шокожопка', price: 1.5 },
-    { name: 'Крокант', price: 0.5 },
-
-];
+testFunc();
 
 // Списки отвечающие за хранение выбранных позиций
 var finalReceipt = [];
