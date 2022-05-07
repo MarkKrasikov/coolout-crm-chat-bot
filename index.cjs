eb894024fd814c320de89f3c15882448b8828f75
Шевяков Максим Андреевич (4056318) <mshevyakov@vtb.ru>
2022-05-07 12:29:05 +0300

    change token

--- index.cjs
+++ index.cjs
@@ -6,7 +6,7 @@ const TOKEN_TEST = '5359120855:AAEhADdpsIOoOV18tj4sNU4q3O6cZi45wbE';
 const TOKEN_AERO = '5396401897:AAHdIGqwHrjFp4K3LRPtFQxB4VaJa7bAsUk';
 
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(TOKEN_TEST, { polling: true });
+const bot = new TelegramBot(TOKEN_AERO, { polling: true });
 
 cron.schedule('*/5 * * * *', () => {
     console.log('Ping at every 5th minute');
