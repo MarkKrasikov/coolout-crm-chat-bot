8ce1e85420f9bffa18efc609ffc4686dee8313cd
Шевяков Максим Андреевич (4056318) <mshevyakov@vtb.ru>
2022-05-17 19:41:17 +0300

    add vercel

--- index.cjs
+++ index.cjs
@@ -11,7 +11,7 @@ const REPORTS_CHAT_AERO = '-1001753751836';
 
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(TOKEN_AERO, { polling: true });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 var isShiftClosed = true;
 var category;
